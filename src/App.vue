21afd46fcfa32b604b450c95426c01ac1036d979
Martijn Michel <martijn.michel@gmail.com>
2020-03-13 08:45:31 +0100

    v

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  mounted() {
+    interface Greet {
+      msg: string;
+      name?: string;
+    }
+
+    class Greeter {
+      greeting: string;
+      name: string;
+      constructor(event: Greet) {
+        this.greeting = event.msg;
+        this.name = event.name ? event.name : 'anon';
+      }
+      greet() {
+        return 'Hello, ' + this.greeting;
+      },
+      bla() { return this.name }
+    }
+
+    let greeter = new Greeter({ msg: 'World' });
+
+    console.log(greeter.bla());
+  }
+});
+</script>
